f3bd82f7b3d35e90e99b515af2de25d875fff3e1
Benj Huser <benj.huser@gmail.com>
2021-03-07 17:07:33 +0100

    Make extension load build definitions

--- plumbr.ts
+++ plumbr.ts
@@ -1,21 +1,31 @@
 /// <reference types="vss-web-extension-sdk" />
 
-VSS.require(["TFS/Build/RestClient"], async function (restClient: any) {
-   var buildClient = restClient.getClient();   
-   var webContext: WebContext = VSS.getWebContext(); 
+define(["require", "exports", "VSS/Service", "TFS/Build/RestClient"],
+   function (require: any, exports: any, VssService: any, TfsBuildRest: any) {
+      var Plumbr: any = (function () {
 
-   var definitions: any[]  = await buildClient.getDefinitions(webContext.project.id);
+         function Plumbr() {
+            console.log("Plumbr constructor");
+         }
 
-   definitions.forEach(definition => {      
-      document.getElementById("buildefinitions").appendChild(new Option(definition.name, `${definition.id}`))
+         Plumbr.prototype.start = async function () {
+            console.log("Plumbr start");
+
+            var self = this;
+            
+            var buildClient = VssService.getCollectionClient(TfsBuildRest.BuildHttpClient);
+
+            var webContext: WebContext = VSS.getWebContext();
+
+            var definitions: any[]  = await buildClient.getDefinitions(webContext.project.id);
+         
+            definitions.forEach(definition => {      
+               console.log(`Build Definition found: ${definition.name}`);
+               document.getElementById("buildefinitions").appendChild(new Option(definition.name, `${definition.id}`))
+            }); 
+         }
+         return Plumbr;
+      })();
+
+      exports.plmbr = new Plumbr();
    });
-});
-
-async function init(): Promise<void> {   
-}
-
-VSS.init(null)
-
-VSS.ready(function () {
-   init();
-});
