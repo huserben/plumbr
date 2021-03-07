1d0b119b784653e5ba95366103f5613ffc2ab3f7
Benj Huser <benj.huser@gmail.com>
2021-03-07 15:21:19 +0100

    Try to get build definitions via client

--- plumbr.ts
+++ plumbr.ts
@@ -1,7 +1,21 @@
 /// <reference types="vss-web-extension-sdk" />
 
+VSS.require(["TFS/Build/RestClient"], async function (restClient: any) {
+   var buildClient = restClient.getClient();   
+   var webContext: WebContext = VSS.getWebContext(); 
+
+   var definitions: any[]  = await buildClient.getDefinitions(webContext.project.id);
+
+   definitions.forEach(definition => {      
+      document.getElementById("buildefinitions").appendChild(new Option(definition.name, `${definition.id}`))
+   });
+});
+
+async function init(): Promise<void> {   
+}
+
 VSS.init(null)
 
-VSS.ready(function() {
-   document.getElementById("name").innerText = VSS.getWebContext().user.name;
+VSS.ready(function () {
+   init();
 });
