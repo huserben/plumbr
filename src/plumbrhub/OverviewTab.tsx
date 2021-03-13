a909326e5c69d8f89089f67ef7f2319cab4eea0e
Benj Huser <benj.huser@gmail.com>
2021-03-13 12:34:48 +0100

    Dynamically load available pipelines

--- src/plumbrhub/OverviewTab.tsx
+++ src/plumbrhub/OverviewTab.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import * as SDK from "azure-devops-extension-sdk";
-import { CommonServiceIds, IProjectPageService, IHostNavigationService, INavigationElement, IPageRoute } from "azure-devops-extension-api";
+import { CommonServiceIds, IProjectPageService, getClient } from "azure-devops-extension-api";
+import * as adoBuild from "azure-devops-extension-api/Build";
+import { BuildRestClient, BuildDefinitionReference } from "azure-devops-extension-api/Build";
+import { Dropdown } from "azure-devops-ui/Dropdown";
+import { IListBoxItem } from "azure-devops-ui/ListBox";
+
 
 export interface IOverviewTabState {
     userName?: string;
     projectName?: string;
-    iframeUrl?: string;
-    extensionData?: string;
-    extensionContext?: SDK.IExtensionContext;
-    host?: SDK.IHostContext;
-    navElements?: INavigationElement[];
-    route?: IPageRoute;
+    pipelines: BuildDefinitionReference[],
+    selectedPipeline?: BuildDefinitionReference
 }
 
 export class OverviewTab extends React.Component<{}, IOverviewTabState> {
@@ -19,7 +20,7 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
         super(props);
 
         this.state = {
-            iframeUrl: window.location.href
+            pipelines: []
         };
     }
 
@@ -29,31 +30,30 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
 
     private async initializeState(): Promise<void> {
         await SDK.ready();
-        
+
         const userName = SDK.getUser().displayName;
         this.setState({
-            userName,
-            extensionContext: SDK.getExtensionContext(),
-            host: SDK.getHost()
-         });
+            userName
+        });
 
         const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
         const project = await projectService.getProject();
-        if (project) {
-            this.setState({ projectName: project.name });
+        if (project) {                        
+            const buildService: BuildRestClient = await getClient(adoBuild.BuildRestClient)
+
+            const buildDefinitions = await buildService.getDefinitions(project.name);
+            
+            this.setState({ projectName: project.name, pipelines: buildDefinitions });
         }
+    }
 
-        const navService = await SDK.getService<IHostNavigationService>(CommonServiceIds.HostNavigationService);
-        const navElements = await navService.getPageNavigationElements();
-        this.setState({ navElements });
-
-        const route = await navService.getPageRoute();
-        this.setState({ route });
+    private onSelectedPipelineChanged = (event: React.SyntheticEvent<HTMLElement>, item: IListBoxItem<BuildDefinitionReference>): void => {
+        this.setState({ selectedPipeline: item.data });
     }
 
     public render(): JSX.Element {
 
-        const { userName, projectName, host, iframeUrl, extensionContext, route, navElements } = this.state;
+        const { userName, projectName, pipelines } = this.state;
 
         return (
             <div className="page-content page-content-top flex-column rhythm-vertical-16">
@@ -62,28 +62,18 @@ export class OverviewTab extends React.Component<{}, IOverviewTabState> {
                     projectName &&
                     <div>Project: {projectName}</div>
                 }
-                <div>iframe URL: {iframeUrl}</div>
-                {
-                    extensionContext &&
-                    <div>
-                        <div>Extension id: {extensionContext.id}</div>
-                        <div>Extension version: {extensionContext.version}</div>
-                    </div>
-                }
-                {
-                    host &&
-                    <div>
-                        <div>Host id: {host.id}</div>
-                        <div>Host name: {host.name}</div>
-                        <div>Host service version: {host.serviceVersion}</div>
-                    </div>
-                }
-                {
-                    navElements && <div>Nav elements: {JSON.stringify(navElements)}</div>
-                }
-                {
-                    route && <div>Route: {JSON.stringify(route)}</div>
-                }
+                <div className="flex-row flex-center">
+                    <label htmlFor="message-level-picker">Pipeline: </label>
+                    <Dropdown<BuildDefinitionReference>
+                        className="margin-left-8"
+                        items={
+                            pipelines.map((pipeline, index) => (
+                                {id: `${pipeline.id}`, text: pipeline.name, data: pipeline }
+                            ))
+                        }
+                        onSelect={this.onSelectedPipelineChanged}
+                    />
+                </div>
             </div>
         );
     }
--- src/plumbrhub/plumbrhub.tsx
+++ src/plumbrhub/plumbrhub.tsx
@@ -10,9 +10,6 @@ import { Page } from "azure-devops-ui/Page";
 import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs";
 
 import { OverviewTab } from "./OverviewTab"; 
-import { NavigationTab } from "./NavigationTab";
-import { ExtensionDataTab } from "./ExtensionDataTab";
-import { MessagesTab } from "./MessagesTab";
 import { showRootComponent } from "../Common";
 
 interface IHubContentState {
@@ -57,9 +54,9 @@ class HubContent extends React.Component<{}, IHubContentState> {
                     tabSize={useCompactPivots ? TabSize.Compact : TabSize.Tall}>
 
                     <Tab name="Overview" id="overview" />
-                    <Tab name="Navigation" id="navigation" />
+                    {/* <Tab name="Navigation" id="navigation" />
                     <Tab name="Extension Data" id="extensionData" />
-                    <Tab name="Messages" id="messages" />
+                    <Tab name="Messages" id="messages" /> */}
                 </TabBar>
 
                 { this.getPageContent() }
@@ -78,7 +75,7 @@ class HubContent extends React.Component<{}, IHubContentState> {
         if (selectedTabId === "overview") {
             return <OverviewTab />;
         }
-        else if (selectedTabId === "navigation") {
+       /* else if (selectedTabId === "navigation") {
             return <NavigationTab />;
         }
         else if (selectedTabId === "extensionData") {
@@ -86,31 +83,11 @@ class HubContent extends React.Component<{}, IHubContentState> {
         }
         else if (selectedTabId === "messages") {
             return <MessagesTab />;
-        }
+        }*/
     }
 
     private getCommandBarItems(): IHeaderCommandBarItem[] {
         return [
-            {
-              id: "panel",
-              text: "Panel",
-              onActivate: () => { this.onPanelClick() },
-              iconProps: {
-                iconName: 'Add'
-              },
-              isPrimary: true,
-              tooltipProps: {
-                text: "Open a panel with custom extension content"
-              }
-            },
-            {
-              id: "messageDialog",
-              text: "Message",
-              onActivate: () => { this.onMessagePromptClick() },
-              tooltipProps: {
-                text: "Open a simple message dialog"
-              }
-            },
             {
                 id: "fullScreen",
                 ariaLabel: this.state.fullScreenMode ? "Exit full screen mode" : "Enter full screen mode",
@@ -118,60 +95,8 @@ class HubContent extends React.Component<{}, IHubContentState> {
                     iconName: this.state.fullScreenMode ? "BackToWindow" : "FullScreen"
                 },
                 onActivate: () => { this.onToggleFullScreenMode() }
-            },
-            {
-              id: "customDialog",
-              text: "Custom Dialog",
-              onActivate: () => { this.onCustomPromptClick() },
-              tooltipProps: {
-                text: "Open a dialog with custom extension content"
-              }
             }
         ];
-    }
-
-    private async onMessagePromptClick(): Promise<void> {
-        const dialogService = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-        dialogService.openMessageDialog("Use large title?", {
-            showCancel: true,
-            title: "Message dialog",
-            onClose: (result) => {
-                this.setState({ useLargeTitle: result });
-            }
-        });
-    }
-
-    private async onCustomPromptClick(): Promise<void> {
-        const dialogService = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-        dialogService.openCustomDialog<boolean | undefined>(SDK.getExtensionContext().id + ".panel-content", {
-            title: "Custom dialog",
-            configuration: {
-                message: "Use compact pivots?",
-                initialValue: this.state.useCompactPivots
-            },
-            onClose: (result) => {
-                if (result !== undefined) {
-                    this.setState({ useCompactPivots: result });
-                }
-            }
-        });
-    }
-
-    private async onPanelClick(): Promise<void> {
-        const panelService = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-        panelService.openPanel<boolean | undefined>(SDK.getExtensionContext().id + ".panel-content", {
-            title: "My Panel",
-            description: "Description of my panel",
-            configuration: {
-                message: "Show header description?",
-                initialValue: !!this.state.headerDescription
-            },
-            onClose: (result) => {
-                if (result !== undefined) {
-                    this.setState({ headerDescription: result ? "This is a header description" : undefined });
-                }
-            }
-        });
     }
 
     private async initializeFullScreenState() {
