9b805c36df5e99226fdd2f4d36884efdafbe4672
Benj Huser <benj.huser@gmail.com>
2021-03-17 19:56:24 +0100

    Cleanup settings

--- src/plumbrhub/SettingsTab.tsx
+++ src/plumbrhub/SettingsTab.tsx
@@ -1,46 +1,21 @@
 import * as React from "react";
-import * as SDK from "azure-devops-extension-sdk";
-import { CommonServiceIds, getClient, IExtensionDataManager, IExtensionDataService, IProjectPageService } from "azure-devops-extension-api";
-import * as adoBuild from "azure-devops-extension-api/Build";
 
-import { Button } from "azure-devops-ui/Button";
-import { TextField } from "azure-devops-ui/TextField";
-import { BuildRestClient } from "azure-devops-extension-api/Build";
+import { BuildDefinitionReference } from "azure-devops-extension-api/Build";
 import { Card } from "azure-devops-ui/Card";
-
-export interface IStageSettings {
-    stageId: number
-}
-
-export interface IPipelineSettings {
-    pipelineId: number,
-    pipelineName: string,
-    defaultBranch: string,
-    stageSettings: IStageSettings[]
-}
-
-export interface IPlumbrhubSettings {
-    pipelineSettings: IPipelineSettings[]
-}
+import { BuildService, IBuildService } from "./Services/BuildService";
 
 export interface ISettingsState {
-    settings: IPlumbrhubSettings,
-    ready?: boolean;
+    buildPipelines: BuildDefinitionReference[]
 }
 
 export class SettingsTab extends React.Component<{}, ISettingsState> {
-
-    private dataManager?: IExtensionDataManager;
+    private buildService?: IBuildService;
 
     constructor(props: {}) {
         super(props);
 
-        var defaultSettings: IPlumbrhubSettings = {
-            pipelineSettings: []
-        }
-
         this.state = {
-            settings: defaultSettings
+            buildPipelines: []
         };
     }
 
@@ -49,82 +24,28 @@ export class SettingsTab extends React.Component<{}, ISettingsState> {
     }
 
     private async initializeState(): Promise<void> {
-        await SDK.ready();
+        this.buildService = await BuildService.getInstance();
 
-        const accessToken = await SDK.getAccessToken();
-        const extDataService = await SDK.getService<IExtensionDataService>(CommonServiceIds.ExtensionDataService);
-
-        this.dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);       
-
-        this.dataManager.getValue<IPlumbrhubSettings>("PipelineSettings").then((settings) => {
-            this.setPipelineSettingState(settings);
-        }, () => {
-            this.setPipelineSettingState(this.state.settings);
-        });
+        await this.setPipelineSettingState();
     }
 
-    private async setPipelineSettingState(settings: IPlumbrhubSettings): Promise<void> {
-        const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
-        const project = await projectService.getProject();
+    private async setPipelineSettingState(): Promise<void> {
+        const buildDefinitions = await this.buildService?.getBuildDefinitions() ?? [];
 
-        const buildService: BuildRestClient = await getClient(adoBuild.BuildRestClient)
-        const buildDefinitions = await buildService.getDefinitions(project?.name ?? "");
-
-        buildDefinitions.forEach(buildDef => {
-            if (settings?.pipelineSettings === undefined){
-                settings = {
-                    pipelineSettings: []
-                }
-            }
-            
-            if (!(settings.pipelineSettings.some(s => s.pipelineId === buildDef.id))){
-                settings.pipelineSettings.push({
-                    pipelineId: buildDef.id,
-                    pipelineName: buildDef.name,
-                    defaultBranch: "refs/heads/main",
-                    stageSettings: []
-                });
-            }
-        })
-
-        this.setState({ ready: true, settings: settings })
+        this.setState({ buildPipelines: buildDefinitions });
     }
 
     public render(): JSX.Element {
-        const { settings, ready } = this.state;
+        const { buildPipelines } = this.state;
 
         return (
-            <div className="page-content page-content-top flex-row rhythm-vertical-16">
-                { settings?.pipelineSettings.map((pipelineSetting, index) => (
+            <div className="page-content page-content-top flex-column rhythm-vertical-16">
+                { buildPipelines.map((pipeline, index) => (
                     <Card
-                        titleProps={{ text: pipelineSetting.pipelineName }}>
-                        <TextField
-                            label="Default Branch"
-                            value={pipelineSetting.defaultBranch}
-                            onChange={(e, newValue) => (settings.pipelineSettings[pipelineSetting.pipelineId].defaultBranch = newValue)}
-                            placeholder="default branch"
-                            disabled={!ready}
-                        />
+                        titleProps={{ text: pipeline.name }}>
                     </Card>
                 ))}
-                <Button
-                    text="Save"
-                    primary={true}
-                    onClick={this.onSaveData}
-                    disabled={!ready}
-                />
             </div>
         );
-    }
-
-    private onSaveData = (): void => {
-        const { settings } = this.state;
-        this.setState({ ready: false });
-
-        this.dataManager!.setValue<IPlumbrhubSettings>("PipelineSettings", settings).then(() => {
-            this.setState({
-                ready: true,
-            });
-        });
     }
 }
